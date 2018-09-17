56a2c637a28ad292430ead29d25823165dd7ced3
ittus <vuminhthang.cm@gmail.com>
2018-09-17 10:57:39 +0900

    Fix Index of Array bug

--- Text101/Assets/Scripts/AdventureGame.cs
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -4,33 +4,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AdventureGame : MonoBehaviour {
+public class AdventureGame : MonoBehaviour
+{
 
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
-    
+
     State state;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         state = startingState;
         textComponent.text = state.GetStateStory();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         ManageState();
-	}
+    }
 
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            state = nextStates[0];
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            state = nextStates[1];
-        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            state = nextStates[2];
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) {
+                state = nextStates[index];
+            }
         }
         textComponent.text = state.GetStateStory();
     }
