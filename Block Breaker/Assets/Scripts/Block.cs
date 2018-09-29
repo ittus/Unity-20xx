cfeb2d46d4988b87e94ffd62c6cfe289c2753103
ittus <vuminhthang.cm@gmail.com>
2018-09-30 08:56:56 +0900

    Add Level manager

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class Block : MonoBehaviour {
     [SerializeField] AudioClip breakSound;
 
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionExit2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
